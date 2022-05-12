91a81024ee6f5ee28e7bedc3ba40d479a70ef79b
kleinerZombie <105456687+kleinerZombie@users.noreply.github.com>
2022-05-12 21:31:48 +0200

    Update bluetooth_commands.sh

--- bluetooth_commands.sh
+++ bluetooth_commands.sh
@@ -2,7 +2,7 @@
 
 
 
-function iamblueDaDaBee () {
+function iambluez () {
 
 	cls; rfkill unblock bluetooth; 
 	rfkill && sleep 3; 
@@ -11,24 +11,18 @@ function iamblueDaDaBee () {
 
 	if [ ! $# -eq 0 ] ; then
 	   if [ "$1" = 'headset' ] ; then
-	        f(){ while ! $ bluetoothctl scan on | grep -q "74:45"; do true; done };
-			echo -e "\n ---  I'm blue! Da ba dee, da ba daa!  --- \n"; 
-	        bluetoothctl connect 74:45:CE:7E:90:04;
+	        f(){ while ! $ bluetoothctl scan on | grep -q "00:00:00:00:00:00"; do true; done };
+	        bluetoothctl connect 00:00:00:00:00:00;
 	   elif [ ! "$1" = 'headset' ] && [ ! $# -eq 0 ] ; then
 	        {  f(){ while ! $ bluetoothctl scan on | grep -q "$1"; do true; done };
-	          echo "Pink fluffy unicorn!"; 
 	          bluetoothctl connect "$1";
 	        } || {
-	           echo "error"
+	           echo "Error: Something went wrong."
 	        }
-	        echo "woop woop"
 	        f () { while $ sleep 10; do echo bluetoothctl scan on; done };
            fi
         else
-	    echo "old mcdonald had a farm, heyo heyo oh!";
 	    timeout 10 bluetoothctl scan on; 
-	    
-	    # f () { while $  sleep 10; do echo bluetoothctl scan on; done };
 	fi       
 	   
 }
